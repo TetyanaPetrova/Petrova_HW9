3bb8a2ee4c08536b08eb25b55246822e45e1cd01
TetyanaPetrova <t.petrova@enamine.net>
2024-02-21 17:54:34 +0200

    unsuccess test added

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -2,10 +2,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.Locale;
 
 public class LoginTests {
 
@@ -13,17 +16,21 @@ public class LoginTests {
 
     @BeforeMethod
     public void beforeMethod() {
+       // Locale.setDefault(new Locale("en", "US"));
         driver = new ChromeDriver();
+        // driver = new EdgeDriver();
         driver.manage().window().maximize();
         driver.get("http://prestashop.qatestlab.com.ua/en/authentication?back=my-account#account-creation");
+
     }
 
     @AfterMethod
-    public void afterMethod(){
+    public void afterMethod() {
         driver.quit();
     }
+
     @Test
-    public void loginSuccess(){
+    public void loginSuccess() {
         driver.findElement(By.xpath("//input[contains(@id,'email_create')]")).sendKeys("petrova@yahoo.com");
         driver.findElement(By.xpath("//button[@id='SubmitCreate']")).click();
         try {
@@ -31,10 +38,17 @@ public class LoginTests {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        String textSucces=driver.findElement(By.xpath("//*[@id=\"noSlide\"]/h1")).getText();
+        String textSucces = driver.findElement(By.xpath("//*[@id=\"noSlide\"]/h1")).getText();
         Assert.assertEquals(textSucces, "CREATE AN ACCOUNT");
         //Assert.assertEquals(textSucces, "РЕЄСТРАЦІЯ");
-      // Your personal information//h3
     }
 
+    @Test
+    public void loginUnsuccess() {
+        driver.findElement(By.xpath("//input[contains(@id,'email_create')]")).sendKeys("petrovayahoo.com");
+        driver.findElement(By.xpath("//button[@id='SubmitCreate']")).click();
+        String textSucces = driver.findElement(By.xpath("//ol/li")).getText();
+        Assert.assertEquals(textSucces, "Invalid email address.");
+
+    }
 }
