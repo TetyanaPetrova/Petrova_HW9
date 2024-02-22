8a9082849f5c6b00d12ed8b20e7234080ede2246
TetyanaPetrova <t.petrova@enamine.net>
2024-02-22 18:31:36 +0200

    waiter for unsuccess added

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -2,6 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -36,6 +39,7 @@ public class LoginTests {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
         String textSucces = driver.findElement(By.xpath("//*[@id=\"noSlide\"]/h1")).getText();
         // Assert.assertEquals(textSucces, "CREATE AN ACCOUNT");
        Assert.assertEquals(textSucces, "РЕЄСТРАЦІЯ");
@@ -45,13 +49,12 @@ public class LoginTests {
     public void loginUnsuccess() {
         driver.findElement(By.xpath("//input[contains(@id,'email_create')]")).sendKeys("petrovayahoo.com");
         driver.findElement(By.xpath("//button[@id='SubmitCreate']")).click();
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        String textSucces = driver.findElement(By.xpath("//ol/li")).getText();
-        Assert.assertEquals(textSucces, "Невірна E-mail адреса");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement webElementTextUnSuccess= wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ol/li")));
+        String textUnSucces = driver.findElement(By.xpath("//ol/li")).getText();
+
+        Assert.assertEquals(textUnSucces, "Невірна E-mail адреса");
 
     }
 }
